79b1d2b94f8b8bf1cd60eaf60e232478f1f205f6
Joelma Skalecki dos Santos <133382445+skaleckijoh17@users.noreply.github.com>
2024-08-30 14:40:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const primeiroNome = "Joelma";
+const sobrenome = "Skalecki";
+const idade = 17;
+const altura = 1.60;
+const massa = 60;
+const imc = massa / (altura * altura);
+
+
+console.log(`paciente ${primeiroNome} ${sobrenome} tem a idade, ${idade} a altura ${altura} e a massa ${massa} e seu imc ${imc}`);
+
+
+// NaN - Not a Number
